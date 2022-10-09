984778b6a492153e5260134a0caea5907e861657
Jahagiresh <jahangiresh@code.edu.az>
2022-10-09 04:08:43 -0700

    heroku

--- noelleFinal/server/models/productModel.js
+++ noelleFinal/server/models/productModel.js
@@ -18,7 +18,7 @@ const productSchema = new mongoose.Schema(
         rating: { type: Number, default: 0 },
       },
     ],
-    substance: { type: String, required: true },
+    substance: { type: String },
 
     comments: [
       {
